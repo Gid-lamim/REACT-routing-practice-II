fc77b55d6f98bf67dfb69dec35e281243aacc5c9
Gid.Lamim <gid.lamim@gmail.com>
2022-11-20 22:05:06 -0300

    add routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
+import { Redirect, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      <Switch>
+        <Route path='/quotes' exact>
 
-
-
+        </Route>
+        <Route path='/quotes/:quote-id'>
+          
+        </Route>
+        <Route path='/quotes/new-quote'>
+          
+        </Route>
+      </Switch>
     </div>
   );
 }
