2cfc46fd77d66a935104e837f4b586b46608c71d
Gid.Lamim <gid.lamim@gmail.com>
2022-11-23 16:47:59 -0300

    add some dummy quotes
    they can later be replaced by quotes from a server for example

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,3 +1,13 @@
+//Let's create an array of quotes to simulate some quotes. 
+//Every quote will be an object with an ID, author and the quote itself.
+const DUMMY_QUOTES = [
+    {id:'q1', author: 'Somebody', text:'Whit happens'},
+    {id:'q2', author: 'Oscar Wilde', text:'Be yourself; everyone else is already taken.'},
+    {id:'q3', author: 'Frank Zappa', text:'So many books, so little time.'},
+    {id:'q4', author: 'Marcus Tullius Cicero ', text:'A room without books is like a body without a soul.'},
+    {id:'q5', author: ' Mahatma Gandhi ', text:'Be the change that you wish to see in the world.'}
+];
+
 const Quotes = () => {
 
     return <h1>All quotes page</h1>
