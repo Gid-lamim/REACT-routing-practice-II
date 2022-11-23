bf63e16c41457a2e18e0f9067a1f5ddf9499fa23
Gid.Lamim <gid.lamim@gmail.com>
2022-11-23 17:02:28 -0300

    add a form to the newQuote pages

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,5 +1,18 @@
+import { Fragment } from "react";
+import QuoteForm from "../components/quotes/QuoteForm";
+
+
 const NewQuote = () => {
-    return <h1>New quote page</h1>
+
+    const QuoteSubitionHandler = (props) => {
+        console.log(props);
+    }
+
+    return <Fragment>
+            <h1>New quote page</h1>
+            <QuoteForm onAddQuote={QuoteSubitionHandler}/>
+        </Fragment>
+
 }
 
 export default NewQuote;
