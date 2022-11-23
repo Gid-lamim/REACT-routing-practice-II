9db27f4f54b1683f3de22468133dea0c8830aaf0
Gid.Lamim <gid.lamim@gmail.com>
2022-11-23 16:09:59 -0300

    add useParams hook to the page QuoteDetails

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,17 @@
+import { Fragment } from "react";
+import { Route, useParams } from "react-router-dom";
+
 const QuoteDetail = () => {
-    return <h1>Quote detail here</h1>
+    const params = useParams();
+
+
+    return(<Fragment>
+                <h1>Quote detail here</h1>
+
+                <p>Showing details for the qute: {params.quoteId}</p>
+
+                
+           </Fragment>);
 }
 
 export default QuoteDetail;
