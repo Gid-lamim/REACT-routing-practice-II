0203d03208211650acedcb039a1c095fd5f297e4
Gid.Lamim <gid.lamim@gmail.com>
2022-11-25 13:37:59 -0300

    add a button to sort the quotes
    it's using query parameters

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,11 +1,21 @@
 import { Fragment } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
 const QuoteList = (props) => {
+  const history = useHistory();
+
+  const onSortingHandler = () => {
+    history.push('/quotes?sort=asc');
+  }
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={onSortingHandler}>Sort quotes</button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
