8249d271156f598f88347c2b835f942722146ec4
Gid.Lamim <gid.lamim@gmail.com>
2022-11-24 21:08:21 -0300

    add programmatic navigation
    programmatic navigation navigates the user away onto another page.
    in this case, after the form is submitted, it navigates the user away from the new-quote page nto the main page.

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,11 +1,22 @@
 import { Fragment } from "react";
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
+import Quotes from "./Quotes";
 
 
 const NewQuote = () => {
+    const history = useHistory();
 
     const QuoteSubitionHandler = (props) => {
         console.log(props);
+
+        //Now here I'll use another hook, the useHistory.
+        //this way, upon subbimition, the user will be navigated away onto another page.
+        //https://v5.reactrouter.com/web/api/Hooks
+
+        history.push("/quotes");
+        //push allows the user to go back.
+
     }
 
     return <Fragment>
